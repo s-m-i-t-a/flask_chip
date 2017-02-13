199d86840967689f67f2f1570b371d725bd4ed41
Jindrich K. Smitka <jsmitka@smita.info>
2017-02-13 23:36:20 +0100

    Refactored token verify and generation

---
+++ flask_chip/tokens.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from typing import Any  # noqa
+
+from itsdangerous import SignatureExpired, BadSignature, TimedJSONWebSignatureSerializer as Serializer
+
+
+def generate(data, key, expiration=24 * 60 * 60):
+    # type: (Any, str, int) -> str
+    s = Serializer(key, expires_in=expiration)
+    token = s.dumps(data)
+    return token.decode('utf-8')
+
+
+def verify(token, key):
+    # type: (str, str) -> Any
+    s = Serializer(key)
+    try:
+        data = s.loads(token)
+    except (SignatureExpired, BadSignature):
+        return None
+
+    return data
--- flask_chip/utils.py
+++ flask_chip/utils.py
@@ -1,28 +1,11 @@
 # -*- coding: utf-8 -*-
 import json
 
-from flask import Response, current_app as app
-
-from itsdangerous import SignatureExpired, BadSignature, TimedJSONWebSignatureSerializer as Serializer
-
-
-def generate_token(user, expiration=24*60*60):
-    s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
-    token = s.dumps({'id': str(user.id)})
-    return token.decode('ascii')
-
-
-def verify_token(token):
-    s = Serializer(app.config['SECRET_KEY'])
-    try:
-        data = s.loads(token)
-    except (SignatureExpired, BadSignature):
-        return None
-
-    return data
+from flask import Response
 
 
 def unauthorized():
+    # type: () -> Response
     data = {
         'message': 'Unauthorized',
         'status': 401
