ba9b8850283fa36d57fe28387f5c6a04dde3282c
Jindrich K. Smitka <jsmitka@smita.info>
2018-01-29 14:46:09 +0100

    Fixed tokens tests

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -5,9 +5,7 @@ import time
 import pytest  # type: ignore
 import six
 
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-
-from flask_chip.tokens import generate, verify
+from flask_chip.tokens import generate, verify_to_value
 
 
 @pytest.fixture
@@ -27,28 +25,25 @@ def token(data, key):
 
 @pytest.fixture
 def expired_token(data, key):
-    return generate(data, key, 0)
+    return generate(data, key, exp=0)
 
 
-def test_generated_token_is_string(key):
-    data = {'foo': ['bar', 'baz']}
+def test_generated_token_is_string(data, key):
     token = generate(data, key)
 
     assert isinstance(token, six.string_types)
 
 
-def test_generated_token_contains_usrer_id(key):
-    data = {'foo': ['bar', 'baz']}
+def test_generated_token_contains_user_id(data, key):
     token = generate(data, key)
 
-    s = Serializer(key)
-    result = s.loads(token)
+    result = verify_to_value(token, key)
 
     assert result == data
 
 
 def test_return_stored_data_when_token_is_right(data, key, token):
-    result = verify(token, key)
+    result = verify_to_value(token, key)
 
     assert result is not None
     assert result == data
@@ -57,12 +52,12 @@ def test_return_stored_data_when_token_is_right(data, key, token):
 def test_return_none_when_token_expire(key, expired_token):
     time.sleep(1)
 
-    result = verify(expired_token, key)
+    result = verify_to_value(expired_token, key)
 
     assert result is None
 
 
 def test_return_none_when_token_is_wrong(key):
-    result = verify('', key)
+    result = verify_to_value('', key)
 
     assert result is None
